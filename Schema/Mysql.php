77cb0af81bec927a5ba9b1cd0cdc25c9a925ccad
Chaosmeister <13346344+Chaosmeister@users.noreply.github.com>
2024-11-25 10:11:58 +0100

    Update Mysql.php

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -11,11 +11,11 @@ function version_1(PDO $pdo)
     $pdo->exec("
     CREATE TABLE definition_of_done (
         id INT NOT NULL AUTO_INCREMENT,
-        `title` TEXT,
+        'title' TEXT,
         status INT,
         task_id INT,
         user_id INT,
-        `text` TEXT,
+        'text' TEXT,
         position INT,
         PRIMARY KEY (id),
         CONSTRAINT definition_of_done_task_id FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE ON UPDATE CASCADE,
