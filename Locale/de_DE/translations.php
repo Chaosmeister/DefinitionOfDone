b7dcf29c29e8133acdfd4af8d425106f06118d8b
RainerBielefeld <RainerBielefeldNG@bielefeldundbuss.de>
2024-11-30 21:48:58 +0100

    Update translations.php
    Typo

--- Locale/de_DE/translations.php
+++ Locale/de_DE/translations.php
@@ -7,7 +7,7 @@ return [
     'Edit row' => 'Zeile bearbeiten',
     'Add row' => 'Zeile anfügen',
     'Toggle state' => 'Erledigungsstatus umschalten',
-    'Close row' => 'Schließen',
+    'Close row' => 'Zeile schließen',
     'Import' => 'Import',
     'Export' => 'Export',
     'Save Table' => 'Speichern',
